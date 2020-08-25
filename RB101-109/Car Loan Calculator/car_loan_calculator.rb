fa99137885a764996cd672a04894bdcc423f1f64
minhphanhvu <minhphanh.vu@gmail.com>
2020-08-25 09:17:30 -0500

    Edit car loan calculator and fix indentation for all rb files

--- RB101-109/Car Loan Calculator/car_loan_calculator.rb
+++ RB101-109/Car Loan Calculator/car_loan_calculator.rb
@@ -1,75 +1,100 @@
-  require 'yaml'
-  MESSAGES = YAML.load_file("car_loan_calculator_messages.yml")
+require 'yaml'
+MESSAGES = YAML.load_file("car_loan_calculator_messages.yml")
 
-  def prompt(message)
-    puts ">>#{message}"
-  end
+def sys_clear
+  system "clear"
+end
 
-  def integer?(number)
-    number.to_i.to_s == number
-  end
+def prompt(message)
+  puts ">> #{message}"
+end
 
-  def float?(number)
-    number.to_f.to_s == number
-  end
+def integer?(number)
+  number.to_i.to_s == number
+end
 
-  def valid_number?(annual_rate)
-    integer?(annual_rate) || float?(annual_rate)
-  end
+def float?(number)
+  number.to_f.to_s == number
+end
 
+def valid_number?(annual_rate)
+  integer?(annual_rate) || float?(annual_rate)
+end
+
+def display_welcome
   prompt(MESSAGES["welcome"])
+end
+
+def retrieve_loan_amount
+  loan_amount = ''
   loop do
-    prompt("What is the Annual Percentage Rate (APR)")
-    prompt(MESSAGES["ask_user_number"])
-    prompt(MESSAGES["example_input"])
-
-    annual_percentage_rate = ""
-    loop do
-      prompt(MESSAGES["ask_APR"])
-      annual_percentage_rate = gets.chomp
-      if valid_number?(annual_percentage_rate)
-        break
-      else
-        prompt(MESSAGES["invalid_APR"])
-      end
-    end
-
-    annual_percentage_rate = annual_percentage_rate.to_f
-    monthly_rate = annual_percentage_rate / (100 * 12)
-
-    loan_amount = ""
-    loop do
-      prompt(MESSAGES["ask_loan_amount"])
-      loan_amount = gets.chomp
-      if valid_number?(loan_amount)
-        break
-      else
-        prompt(MESSAGES["invalid_loan_amount"])
-      end
-    end
-
-    prompt("We need loan duration in months")
-    loan_duration = ""
-    loop do
-      prompt(MESSAGES["ask_loan_duration"])
-      loan_duration = gets.chomp
-      if integer?(loan_duration)
-        break
-      else
-        prompt(MESSAGES["invalid_loan_duration"])
-      end
-    end
-
-    loan_duration = loan_duration.to_f
-    loan_amount = loan_amount.to_f
-    monthly_payment = loan_amount * (monthly_rate / (1 - (1 + monthly_rate)**(-loan_duration)))
-    puts "Your monthly payment is: #{format('%.2f', monthly_payment)}"
-
-    prompt(MESSAGES["calculate_again?"])
-    answer = gets.chomp
-    if answer.downcase() == "y"
-      next
-    else
+    prompt(MESSAGES["ask_loan_amount"])
+    loan_amount = gets.chomp
+    if valid_number?(loan_amount)
       break
+    else
+      prompt(MESSAGES["invalid_loan_amount"])
     end
   end
+  loan_amount
+end
+
+def retrieve_apr
+  annual_percentage_rate = ''
+  loop do
+    prompt(MESSAGES["ask_APR"])
+    annual_percentage_rate = gets.chomp
+    if valid_number?(annual_percentage_rate)
+      break
+    else
+      prompt(MESSAGES["invalid_APR"])
+    end
+  end
+  annual_percentage_rate
+end
+
+def retrieve_duration
+  loan_duration = ''
+  loop do
+    prompt(MESSAGES["ask_loan_duration"])
+    loan_duration = gets.chomp
+    if integer?(loan_duration)
+      break
+    else
+      prompt(MESSAGES["invalid_loan_duration"])
+    end
+  end
+  loan_duration
+end
+
+loop do
+  sys_clear
+  display_welcome
+  prompt("What is the Annual Percentage Rate (APR)")
+  prompt(MESSAGES["ask_user_number"])
+  prompt(MESSAGES["example_input"])
+
+  annual_percentage_rate = retrieve_apr
+
+  annual_percentage_rate = annual_percentage_rate.to_f
+  monthly_rate = annual_percentage_rate / (100 * 12)
+
+  loan_amount = retrieve_loan_amount
+
+  prompt("We need loan duration in months")
+  loan_duration = retrieve_duration
+
+  loan_duration = loan_duration.to_f
+  loan_amount = loan_amount.to_f
+  monthly_payment = loan_amount * (monthly_rate / (1 -
+                    (1 + monthly_rate)**(-loan_duration)))
+  puts "Your monthly payment is: #{format('%.2f', monthly_payment)}"
+
+  prompt(MESSAGES["calculate_again?"])
+  answer = gets.chomp
+  if answer.downcase == "y" || answer.downcase == "yes"
+    next
+  else
+    break
+  end
+end
--- RB101-109/calculator.rb
+++ RB101-109/calculator.rb
@@ -1,111 +1,111 @@
-  require 'yaml'
-  MESSAGES = YAML.load_file('calculator_messages.yml')
+require 'yaml'
+MESSAGES = YAML.load_file('calculator_messages.yml')
 
-  def prompt(message)
-    puts("=> #{message}")
-  end
-
-  prompt(MESSAGES['welcome'])
-
-  def operation_to_message(op)
-    result = case op
-    when '1'
-      "Adding"
-    when '2'
-      "Subtracting"
-    when "3"
-      "Multiplying"
-    when "4"
-      "Dividing"
-    end
-    result
-  end
-
-  name = ""
-  loop do
-    name = Kernel.gets().chomp()
-
-    if name.empty?()
-      prompt(MESSAGES['valid_name'])
-    else
-      break
-    end
-  end
-
-  def valid_number?(num)
-    /^\d+$/.match(num)
-  end
-
-  prompt("Hi #{name}")
-
-loop do
-  number1 = ''
-  loop do
-    prompt(MESSAGES['first_number'])
-    number1 = Kernel.gets().chomp()
-
-    if valid_number?(number1)
-      break
-    else
-      prompt(MESSAGES['valid_number'])
-    end
-  end
-
-  number2 = ''
-  loop do
-    prompt(MESSAGES['second_number'])
-    number2 = Kernel.gets().chomp()
-
-    if valid_number?(number2)
-      break
-    else
-      prompt(MESSAGES['valid_number'])
-    end
-  end
-
-  operator_prompt = <<-MSG
-  What operator would you like to perform?
-    1) add
-    2) subtraction
-    3) Multiply
-    4) Divide
-  MSG
-  prompt(operator_prompt)
-
-  operator = ""
-  loop do
-    operator = Kernel.gets().chomp()
-    if %w(1 2 3 4).include?(operator)
-      break
-    else
-      prompt("Choose one of 1, 2, 3, or 4 please!")
-    end
-  end
-
-  prompt("#{operation_to_message(operator)} the two numbers...")
-
-  result = case operator 
-            when '1'
-              number1.to_i() + number2.to_i()
-            when '2'
-              number1.to_i() - number2.to_i()
-            when '3'
-              number1.to_i() * number2.to_i()
-            when '4'
-              number1.to_i() / number2.to_i()
-           end
-  
-  prompt("Your result is: #{result}")
-  
-  prompt(MESSAGES['ask_calculator'])
-
-  answer = Kernel.gets().chomp()
-  break unless answer.downcase().start_with?('y')
+def prompt(message)
+  puts("=> #{message}")
 end
 
-    
+prompt(MESSAGES['welcome'])
+
+def operation_to_message(op)
+  result = case op
+  when '1'
+    "Adding"
+  when '2'
+    "Subtracting"
+  when "3"
+    "Multiplying"
+  when "4"
+    "Dividing"
+  end
+  result
+end
+
+name = ""
+loop do
+  name = Kernel.gets().chomp()
+
+  if name.empty?()
+    prompt(MESSAGES['valid_name'])
+  else
+    break
+  end
+end
+
+def valid_number?(num)
+  /^\d+$/.match(num)
+end
+
+prompt("Hi #{name}")
+
+loop do
+number1 = ''
+loop do
+  prompt(MESSAGES['first_number'])
+  number1 = Kernel.gets().chomp()
+
+  if valid_number?(number1)
+    break
+  else
+    prompt(MESSAGES['valid_number'])
+  end
+end
+
+number2 = ''
+loop do
+  prompt(MESSAGES['second_number'])
+  number2 = Kernel.gets().chomp()
+
+  if valid_number?(number2)
+    break
+  else
+    prompt(MESSAGES['valid_number'])
+  end
+end
+
+operator_prompt = <<-MSG
+What operator would you like to perform?
+  1) add
+  2) subtraction
+  3) Multiply
+  4) Divide
+MSG
+prompt(operator_prompt)
+
+operator = ""
+loop do
+  operator = Kernel.gets().chomp()
+  if %w(1 2 3 4).include?(operator)
+    break
+  else
+    prompt("Choose one of 1, 2, 3, or 4 please!")
+  end
+end
+
+prompt("#{operation_to_message(operator)} the two numbers...")
+
+result = case operator 
+          when '1'
+            number1.to_i() + number2.to_i()
+          when '2'
+            number1.to_i() - number2.to_i()
+          when '3'
+            number1.to_i() * number2.to_i()
+          when '4'
+            number1.to_i() / number2.to_i()
+          end
+
+prompt("Your result is: #{result}")
+
+prompt(MESSAGES['ask_calculator'])
+
+answer = Kernel.gets().chomp()
+break unless answer.downcase().start_with?('y')
+end
 
   
-  
+
+
+
 
 
