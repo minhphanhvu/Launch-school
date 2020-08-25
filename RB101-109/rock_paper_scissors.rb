84b600e7b90d9c8964aa740a319b895912dce53b
minhphanhvu <minhphanh.vu@gmail.com>
2020-08-25 10:30:42 -0500

    refactor rock paper scissors.rb

--- RB101-109/rock_paper_scissors.rb
+++ RB101-109/rock_paper_scissors.rb
@@ -2,6 +2,10 @@ require 'yaml'
 VALID_CHOICES = %w(rock paper scissors)
 MESSAGES = YAML.load_file("rock_paper_scissors.yml")
 
+def system_clear
+  system "clear"
+end
+
 def prompt(message)
   puts("=> #{message}")
 end
@@ -12,8 +16,8 @@ end
 
 def win?(first, second)
   (first == 'rock' && second == 'scissors') ||
-  (first == 'paper' && second == 'rock') ||
-  (first == 'scissors' && second == 'paper')
+    (first == 'paper' && second == 'rock') ||
+    (first == 'scissors' && second == 'paper')
 end
 
 def display_result(player, computer)
@@ -25,32 +29,49 @@ def display_result(player, computer)
     prompt("You lose!")
   end
 end
-loop do
-  def player_choose
-    choice = ''
-    loop do
-      prompt("Choose one: rock, paper, scissors")
-      choice = gets.chomp
-      if valid_input?(choice)
-        break
-      else
-        prompt("Invalid choice, please input again")
-      end
-    end
-    choice
-  end
 
+def player_choose
+  choice = ''
   loop do
-    choice = player_choose
-
-    computer_choice = %w(rock paper scissors).sample
-
-    puts("You choose #{choice} and computer choice: #{computer_choice}")
-
-    display_result(choice, computer_choice)
+    prompt(MESSAGES["option_choose"])
+    choice = gets.chomp
+    if valid_input?(choice)
+      break
+    else
+      prompt(MESSAGES["invalid_choice"])
+    end
   end
-  prompt("Do you want to play again? yes or y for yes, otherwise no")
-  answer = gets.chomp
-  break if answer.downcase != 'y' || answer.downcase != 'yes'
+  choice
 end
-prompt("Thank you for playing!")
+
+def computer_choose
+  %w(rock paper scissors).sample
+end
+
+def display_choices(player_choice, computer_choice)
+  puts("You choose #{player_choice} and computer choice: #{computer_choice}")
+end
+
+def play_again?
+  prompt(MESSAGES["play_again"])
+  answer = gets.chomp
+  answer.downcase == 'y' || answer.downcase == 'yes'
+end
+
+loop do
+  system_clear
+
+  choice = player_choose
+
+  computer_choice = computer_choose
+
+  display_choices(choice, computer_choice)
+
+  display_result(choice, computer_choice)
+
+  break unless play_again?
+end
+
+system_clear
+
+prompt(MESSAGES["goodbye"])
