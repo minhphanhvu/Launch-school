01572dd0a5695444dec2d1b0e61400e229ce3a02
minhphanhvu <minhphanh.vu@gmail.com>
2020-08-25 09:35:31 -0500

    update rock paper scissors game

--- RB101-109/rock_paper_scissors.rb
+++ RB101-109/rock_paper_scissors.rb
@@ -1,4 +1,6 @@
+require 'yaml'
 VALID_CHOICES = %w(rock paper scissors)
+MESSAGES = YAML.load_file("rock_paper_scissors.yml")
 
 def prompt(message)
   puts("=> #{message}")
@@ -8,39 +10,47 @@ def valid_input?(choice)
   VALID_CHOICES.include?(choice)
 end
 
+def win?(first, second)
+  (first == 'rock' && second == 'scissors') ||
+  (first == 'paper' && second == 'rock') ||
+  (first == 'scissors' && second == 'paper')
+end
+
 def display_result(player, computer)
-  if (player == 'rock' && computer == 'scissors') ||
-      (player == 'paper' && computer == 'rock') ||
-      (player == 'scissors' && computer == 'paper')
-    prompt("You won!")
-  elsif player == computer
+  if player == computer
     prompt("Draw!")
+  elsif win?(player, computer)
+    prompt("You win!")
   else
     prompt("You lose!")
   end
 end
-
 loop do
-  choice = ''
-  loop do
-    prompt("Choose one: rock, paper, scissors")
-    choice = gets.chomp
-    if valid_input?(choice)
-      break
-    else
-      prompt("Invalid choice, please input again")
+  def player_choose
+    choice = ''
+    loop do
+      prompt("Choose one: rock, paper, scissors")
+      choice = gets.chomp
+      if valid_input?(choice)
+        break
+      else
+        prompt("Invalid choice, please input again")
+      end
     end
+    choice
   end
 
-  computer_choice = %w(rock paper scissors).sample
+  loop do
+    choice = player_choose
 
-  puts ("You choose #{choice} and computer choice: #{computer_choice}")
+    computer_choice = %w(rock paper scissors).sample
 
-  display_result(choice, computer_choice)
+    puts("You choose #{choice} and computer choice: #{computer_choice}")
 
-  prompt("Do you want to play again? Y or y for yes, otherwise no")
+    display_result(choice, computer_choice)
+  end
+  prompt("Do you want to play again? yes or y for yes, otherwise no")
   answer = gets.chomp
-  break if answer.downcase != 'y'
+  break if answer.downcase != 'y' || answer.downcase != 'yes'
 end
-
 prompt("Thank you for playing!")
