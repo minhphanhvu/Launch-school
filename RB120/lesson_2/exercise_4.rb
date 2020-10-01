497c7adbd1d64018819aa4e06aa0bf321c4784f9
minhphanhvu <minhphanh.vu@gmail.com>
2020-09-30 20:27:56 -0500

    exercise_4

---
+++ RB120/lesson_2/exercise_4.rb
@@ -0,0 +1,34 @@
+# class Person
+#   def name=(name)
+#     @name = name.downcase.capitalize
+#   end
+#   attr_reader :name
+# end
+
+# person1 = Person.new
+# person1.name = 'eLiZaBeTh'
+# puts person1.name
+
+# class Person
+#   def initialize(name)
+#     @name = name
+#   end
+
+#   def name
+#     @name.clone
+#   end
+# end
+
+# person1 = Person.new('James')
+# person1.name.reverse!
+# puts person1.name
+
+class Person
+end
+
+person1 = Person.new
+person1.age = 20
+puts person1.age
+
+
+
