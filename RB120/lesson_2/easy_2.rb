bcaaab44cdeb30e4449797abeb82b1d1c1125988
minhphanhvu <minhphanh.vu@gmail.com>
2020-10-03 15:55:18 -0500

    finish easy_2

--- RB120/lesson_2/easy_2.rb
+++ RB120/lesson_2/easy_2.rb
@@ -121,9 +121,54 @@ else
   puts 'Bill and Penny have the same amount of money.'
 end
 
+p "----"
 # p7
 
 class Pet
+  attr_reader :species, :name
+  def initialize(species, name)
+    @species = species
+    @name = name
+  end
+end
+
+class Owner
+  attr_reader :name, :pets
+  def initialize(name)
+    @name = name
+    @pets = []
+  end
+
+  def number_of_pets  
+    @pets.size
+  end
+end
+
+class Shelter
+  attr_reader :register
+  def initialize
+    @register = []
+  end
+
+  def adopt(owner, pet)
+    if !register.include?(owner)
+      @register << owner
+      owner.pets << pet
+    else
+      owner.pets << pet
+    end
+  end
+
+  def print_adoptions
+    @register.each do |owner|
+      puts "#{owner.name} has adopted the following pets:"
+      owner.pets.each do |pet|
+        puts "a #{pet.species} named #{pet.name}"
+      end
+      puts
+    end
+  end
+end
 
 butterscotch = Pet.new('cat', 'Butterscotch')
 pudding      = Pet.new('cat', 'Pudding')
@@ -162,6 +207,114 @@ puts "#{bholmes.name} has #{bholmes.number_of_pets} adopted pets."
 # P Hanson has 3 adopted pets.
 # B Holmes has 4 adopted pets.
 
+# p8
+
+class Expander
+  def initialize(string)
+    @string = string
+  end
+
+  def to_s
+    expand(3)
+  end
+
+  private
+
+  def expand(n)
+    @string * n
+  end
+end
+
+expander = Expander.new('xyz')
+puts expander
+
+# p9
+
+module Walkable
+  def walk
+    "#{name} #{gait} forward"
+  end
+end
+
+class Person
+  include Walkable
+  attr_reader :name
+
+  def initialize(name)
+    @name = name
+  end
+
+  private
+
+  def gait
+    "strolls"
+  end
+end
+
+class Cat
+  include Walkable
+  attr_reader :name
+
+  def initialize(name)
+    @name = name
+  end
+
+  private
+
+  def gait
+    "saunters"
+  end
+end
+
+class Cheetah
+  include Walkable
+  attr_reader :name
+
+  def initialize(name)
+    @name = name
+  end
+
+  private
+
+  def gait
+    "runs"
+  end
+end
+
+mike = Person.new("Mike")
+mike.walk
+# => "Mike strolls forward"
+
+kitty = Cat.new("Kitty")
+kitty.walk
+# => "Kitty saunters forward"
+
+flash = Cheetah.new("Flash")
+flash.walk
+# => "Flash runs forward"
+
+# p10
+
+class Noble
+  attr_reader :name, :title
+
+  include Walkable
+
+  def initialize(name, title)
+    @title = title
+    @name = name
+  end
+
+  def to_s
+    "#{title} #{name}"
+  end
+
+  private
+
+  def gait
+    "struts"
+  end
+end
 
 
 
