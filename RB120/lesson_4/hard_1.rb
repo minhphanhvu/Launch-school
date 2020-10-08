d22359d2e5406c79cbc635c9a1f9ba6d99fe0cc7
minhphanhvu <minhphanh.vu@gmail.com>
2020-10-08 14:06:05 -0500

    hard_1 complete

--- RB120/lesson_4/hard_1.rb
+++ RB120/lesson_4/hard_1.rb
@@ -20,8 +20,15 @@ end
 
 # p2
 
+module Moveable
+  attr_accessor: :speed, :heading
+  def range
+    @fuel_efficiency * @fuel_capacity
+  end
+end
+
 class WheeledVehicle
-  attr_accessor :speed, :heading
+  include Moveable
 
   def initialize(tire_array, km_traveled_per_liter, liters_of_fuel_capacity)
     @tires = tire_array
@@ -53,5 +60,38 @@ class Motorcycle < WheeledVehicle
   def initialize
     # 2 tires are various tire pressures
     super([20,20], 80, 8.0)
+  end
+end
+
+class Catamaran
+  include Moveable
+
+  attr_reader :propeller_count, :hull_count
+
+  def initialize(num_propellers, num_hulls, km_traveled_per_liter, liters_of_fuel_capacity)
+    @km_traveled_per_liter = km_traveled_per_liter
+    @liters_of_fuel_capacity = liters_of_fuel_capacity
+  end
+
+  def range
+    super + 10
+  end
+end
+
+# p4
+
+class Motorboat
+  include Moveable
+  
+  attr_reader :propeller_count, :hull_count
+  def initialize(km_traveled_per_liter, liters_of_fuel_capacity)
+    @propeller_count = 1
+    @hull_count = 1
+    @km_traveled_per_liter = km_traveled_per_liter
+    @liters_of_fuel_capacity = liters_of_fuel_capacity
+  end
+
+  def range
+    super + 10
   end
 end
