8b2a83fab1bdc1b5d2a9bc3e7d0c553a3ed24cee
minhphanhvu <minhphanh.vu@gmail.com>
2020-10-26 06:21:37 -0500

    each and select method

--- RB130/lesson_1/todolist.rb
+++ RB130/lesson_1/todolist.rb
@@ -71,93 +71,86 @@ class TodoList
   end
 
   def to_a
-    
+    todos.clone
+  end
+
+  def done?
+    todos.each do |todo|
+      return false if todo.done?
+    end
+    true
+  end
+
+  def item_at(index)
+    raise IndexError if index >= todos.size || index < 0
+    todos[index]
+  end
+
+  def mark_done_at(index)
+    raise IndexError if index >= todos.size || index < 0
+    todos[index].done!
+  end
+
+  def mark_undone_at(index)
+    raise IndexError if index >= todos.size || index < 0
+    todos[index].undone!
+  end
+
+  def done!
+    todos.each { |todo| todo.done! }
+  end
+
+  def shift
+    todos.shift
+  end
+
+  def pop
+    todos.pop
+  end
+
+  def remove_at(index)
+    raise IndexError if index >= todos.size || index < 0
+    todos.delete_at(index)
+  end
+
+  def to_s
+    text = "---#{title}----"
+    todos.each { |todo| puts todo }
+    text
+  end
+
+  def each
+    counter = 0
+
+    while counter < todos.size
+      yield(todos[counter])
+      counter += 1
+    end
+
+    self
+  end
+
+  def select
+    results = []
+
+    each do |todo|
+      results << todo if yield(todo)
+    end
+    results
   end
 end
-
-# given
+# ---- Adding to the list -----
 todo1 = Todo.new("Buy milk")
 todo2 = Todo.new("Clean room")
 todo3 = Todo.new("Go to gym")
+
 list = TodoList.new("Today's Todos")
+list.add(todo1)
+list.add(todo2)
+list.add(todo3)
 
-# ---- Adding to the list -----
+todo1.done!
 
-# add
-list.add(todo1)                 # adds todo1 to end of list, returns list
-list.add(todo2)                 # adds todo2 to end of list, returns list
-list.add(todo3)                 # adds todo3 to end of list, returns list
-#p list.todos
-#list.add(1)                     # raises TypeError with message "Can only add Todo objects"
+results = list.select { |todo| todo.done? }    # you need to implement this method
 
-# <<
-# same behavior as add
-
-# ---- Interrogating the list -----
-
-# size
-list.size                       # returns 3
-
-# first
-list.first                      # returns todo1, which is the first item in the list
-
-# last
-list.last                       # returns todo3, which is the last item in the list
-
-#to_a
-list.to_a                      # returns an array of all items in the list
-
-#done?
-list.done?                     # returns true if all todos in the list are done, otherwise false
-
-# ---- Retrieving an item in the list ----
-
-# item_at
-list.item_at                    # raises ArgumentError
-list.item_at(1)                 # returns 2nd item in list (zero based index)
-list.item_at(100)               # raises IndexError
-
-# ---- Marking items in the list -----
-
-# mark_done_at
-list.mark_done_at               # raises ArgumentError
-list.mark_done_at(1)            # marks the 2nd item as done
-list.mark_done_at(100)          # raises IndexError
-
-# mark_undone_at
-list.mark_undone_at             # raises ArgumentError
-list.mark_undone_at(1)          # marks the 2nd item as not done,
-list.mark_undone_at(100)        # raises IndexError
-
-# done!
-list.done!                      # marks all items as done
-
-# ---- Deleting from the list -----
-
-# shift
-list.shift                      # removes and returns the first item in list
-
-# pop
-list.pop                        # removes and returns the last item in list
-
-# remove_at
-list.remove_at                  # raises ArgumentError
-list.remove_at(1)               # removes and returns the 2nd item
-list.remove_at(100)             # raises IndexError
-
-# ---- Outputting the list -----
-
-# to_s
-list.to_s                      # returns string representation of the list
-
-# ---- Today's Todos ----
-# [ ] Buy milk
-# [ ] Clean room
-# [ ] Go to gym
-
-# or, if any todos are done
-
-# ---- Today's Todos ----
-# [ ] Buy milk
-# [X] Clean room
-# [ ] Go to gym
+puts results.inspect
