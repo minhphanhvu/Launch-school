24580995acecf3051100447a7230f901c3c7d59b
minhphanhvu <minhphanh.vu@gmail.com>
2020-11-15 19:36:24 -0600

    finalize phone_number

--- RB130/challenges/medium_1/phone_number.rb
+++ RB130/challenges/medium_1/phone_number.rb
@@ -6,15 +6,36 @@ class PhoneNumber
   end
 
   def number
-    convert_nums = convert
-    convert_nums.size == 10 ? self.str_nums = convert_nums : self.str_nums = '0000000000'
+    if bad_number?
+      self.str_nums = '0000000000'
+    elsif str_nums.size == 11
+      self.str_nums = str_nums[1..-1]
+    else
+      self.str_nums = convert
+    end
+  end
+
+  def area_code
+    str_nums[0..2]
   end
 
   def to_s
-    str_nums
+    number
+    "(" + str_nums[0..2] + ") " + str_nums[3..5] + "-" + str_nums[6..-1]
   end
 
   private
+
+  def bad_number?
+    if convert.size < 10 ||
+       convert.size > 11 ||
+       str_nums.scan(/[A-Za-z]/).size > 0
+      return true
+    elsif convert.size == 11 && !convert.start_with?('1')
+      return true
+    end
+    false
+  end
 
   def convert
     str_nums.scan(/\d/).join
