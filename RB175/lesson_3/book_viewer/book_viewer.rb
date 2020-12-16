ca9c05df8406acb3aa7f9c837eacb0d9f79f8336
minhphanhvu <minhphanh.vu@gmail.com>
2020-12-15 23:15:41 -0600

    dynamic_list app

--- RB175/lesson_3/book_viewer/book_viewer.rb
+++ RB175/lesson_3/book_viewer/book_viewer.rb
@@ -1,6 +1,18 @@
+require 'tilt/erubis'
 require "sinatra"
 require "sinatra/reloader"
 
 get "/" do
-  File.read "public/template.html"
+  @chapters = File.readlines 'data/toc.txt'
+  @title = "The Adventures of Sherlock Holmes"
+
+  erb :home
+end
+
+get "/chapters/1" do
+  @title = "Chapter 1" 
+  @chapter_1 = File.read 'data/chp1.txt'
+  @chapters = File.readlines 'data/toc.txt'
+
+  erb :chapter
 end
---
+++ RB175/lesson_3/dynamic_challenge/dynamic_list.rb
@@ -0,0 +1,11 @@
+require "tilt/erubis"
+require 'sinatra'
+require "sinatra/reloader"
+
+get "/" do
+  @list = Dir.glob("public/*").map {|file| File.basename(file, ".*") }.sort
+  @title = "Dynamic app"
+  @list.reverse! if params[:sort] == "desc"
+
+  erb :home
+end
