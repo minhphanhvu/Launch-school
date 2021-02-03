d07c77424138c12479ac470af9251c1d79842603
minhphanhvu <minhphanh.vu@gmail.com>
2021-02-02 21:29:26 -0600

    init expense database project

---
+++ RB185/expense/schema.sql
@@ -0,0 +1,12 @@
+CREATE TABLE expenses (
+  id serial PRIMARY KEY,
+  amount numeric(6, 2) NOT NULL,
+  memo text NOT NULL,
+  created_on timestamp NOT NULL
+);
+
+ALTER TABLE expenses ADD CONSTRAINT positive_amount CHECK (amount >= 0.01);
+
+INSERT INTO expenses (amount, memo, created_on) VALUES (14.56, 'Pencils', NOW());
+INSERT INTO expenses (amount, memo, created_on) VALUES (3.29, 'Coffee', NOW());
+INSERT INTO expenses (amount, memo, created_on) VALUES (49.99, 'Text Editor', NOW());
