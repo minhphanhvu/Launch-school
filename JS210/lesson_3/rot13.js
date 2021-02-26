72d14867dc08fdf7054d142ae16d4d972aa50447
minhphanhvu <minhphanh.vu@gmail.com>
2021-02-26 02:32:40 -0600

    refactor

--- JS210/lesson_3/rot13.js
+++ JS210/lesson_3/rot13.js
@@ -1,14 +1,15 @@
 function rot13(string) {
-
+  const ROT_NUM = 13;
   let charCode = '';
   let result = '';
 
   for (let i = 0; i < string.length; i++) {
     charCode = string[i].charCodeAt(0);
+
     if (charCode >= 65 && charCode <= 90) {
-      charCode = ((charCode - 65) + 13) % 26 + 65; 
+      charCode = ((charCode - 65) + ROT_NUM) % 26 + 65; 
     } else if (charCode >= 97 && charCode <= 122) {
-      charCode = ((charCode - 97) + 13) % 26 + 97;
+      charCode = ((charCode - 97) + ROT_NUM) % 26 + 97;
     }
 
     result += String.fromCharCode(charCode);
