6a708ddc9470f4c3762420e8128b3d69a4a7160c
minhphanhvu <minhphanh.vu@gmail.com>
2021-03-01 18:54:40 -0600

    add array exercises

---
+++ JS210/lesson_4/array.js
@@ -0,0 +1,172 @@
+// Array Copy 1
+
+let myArray = [1, 2, 3, 4];
+const myOtherArray = myArray;
+
+myArray.pop(); 
+console.log(myArray); // [1, 2, 3]
+console.log(myOtherArray); // [1, 2, 3]
+
+myArray = [1, 2];
+console.log(myArray); // [1, 2]
+console.log(myOtherArray); // [1, 2, 3]
+
+// Array Copy part 2
+
+let myArray2 = [1, 2, 3, 4];
+const myOtherArray2 = myArray2.slice();
+
+myArray2.pop();
+console.log(myArray2);
+console.log(myOtherArray2);
+
+// Array Concat part 1
+
+function concat(array1, secondArgument) {
+  let newArray = array1
+
+  if (Array.isArray(secondArgument)) {
+    for (let i = 0; i < secondArgument.length; i++) {
+      newArray.push(secondArgument[i]);
+    }
+  } else {
+    newArray.push(secondArgument);
+  }
+
+  return newArray;
+}
+
+console.log('Array Concat part 1')
+console.log(concat([1, 2, 3], [4, 5, 6]));          // [1, 2, 3, 4, 5, 6]
+console.log(concat([1, 2], 3));                     // [1, 2, 3]
+console.log(concat([2, 3], ['two', 'three']));      // [2, 3, "two", "three"]
+console.log(concat([2, 3], 'four'));                // [2, 3, "four"]
+
+
+const obj = { a: 2, b: 3 };
+const newArray = concat([2, 3], obj);
+console.log(newArray);                              // [2, 3, { a: 2, b: 3 }]
+obj.a = 'two';
+console.log(newArray);                              // [2, 3, { a: "two", b: 3 }]
+
+const arr1 = [1, 2, 3];
+const arr2 = [4, 5, obj];
+const arr3 = concat(arr1, arr2);
+console.log(arr3);                                  // [1, 2, 3, 4, 5, { a: "two", b: 3 }]
+obj.b = 'three';
+console.log(arr3);                                  // [1, 2, 3, 4, 5, { a: "two", b: "three" }]
+
+console.log(arr3[5].b = 3);                         // or, `arr3[5]['b'] = 3;`
+console.log(obj);                                   // { a: "two", b: 3 }
+
+// Array Concat part 2
+
+function concat2(arr, ...arrs) {
+  let newArray = arr;
+
+  for (let i = 0; i < arrs.length; i++) {
+    newArray = concat(newArray, arrs[i]);
+  }
+
+  return newArray;
+}
+
+console.log('Array Concat part 2')
+console.log(concat2([1, 2, 3], [4, 5, 6], [7, 8, 9]));    // [1, 2, 3, 4, 5, 6, 7, 8, 9]
+console.log(concat2([1, 2], 'a', ['one', 'two']));        // [1, 2, "a", "one", "two"]
+console.log(concat2([1, 2], ['three'], 4));               // [1, 2, "three", 4]
+
+// Array Pop and Push
+
+function pop(array) {
+  const poppedElement = array[array.length - 1];
+  array.splice(array.length - 1, 1);
+
+  return poppedElement;
+}
+
+console.log('Array Pop and Push');
+
+// pop
+const array1 = [1, 2, 3];
+console.log(pop(array1));            // 3
+console.log(array1);                // [1, 2]
+console.log(pop([]));                           // undefined
+console.log(pop([1, 2, ['a', 'b', 'c']]));      // ["a", "b", "c"]
+
+
+function push(array, ...args) {
+  const length = args.length;
+  const size = array.length;
+
+  for (let i = 0; i < length; i += 1) {
+    array[i + size] = args[i];
+  }
+
+  return array.length;
+}
+
+// push
+const array2 = [1, 2, 3];
+console.log(push(array2, 4, 5, 6));              // 6
+console.log(array2);                // [1, 2, 3, 4, 5, 6]
+console.log(push([1, 2], ['a', 'b']));          // 3
+console.log(push([], 1));                       // 1
+console.log(push([]));                          // 0
+
+// Array and String Reverse
+
+function reverse(inputForReversal) {
+  if (Array.isArray(inputForReversal)) {
+    return reverseArray(inputForReversal);
+  } else {
+    return reverseString(inputForReversal);
+  }
+}
+
+function reverseArray(inputForReversal) {
+  const reversed = [];
+  const length = inputForReversal.length;
+  let index = 0;
+
+  for (let i = length - 1; i >= 0; i -= 1) {
+    reversed[index] = inputForReversal[i];
+    index += 1;
+  }
+
+  return reversed;
+}
+
+function reverseString(inputForReversal) {
+  const stringArray = inputForReversal.split('');
+  return reverseArray(stringArray).join('');
+}
+
+console.log('Array and String Reverse');
+console.log(reverse('Hello'));           // "olleH"
+console.log(reverse('a'));               // "a"
+console.log(reverse([1, 2, 3, 4]));      // [4, 3, 2, 1]
+console.log(reverse([]));                // []
+
+const array = [1, 2, 3];
+console.log(reverse(array));             // [3, 2, 1]
+console.log(array);                      // [1, 2, 3]
+
+// Array Shift and Unshift
+
+
+
+console.log('Array Shift and Unshift');
+shift([1, 2, 3]);                // 1
+shift([]);                       // undefined
+shift([[1, 2, 3], 4, 5]);        // [1, 2, 3]
+
+unshift([1, 2, 3], 5, 6);        // 5
+unshift([1, 2, 3]);              // 3
+unshift([4, 5], [1, 2, 3]);      // 3
+
+const testArray = [1, 2, 3];
+shift(testArray);                // 1
+testArray;                       // [2, 3]
+unshift(testArray, 5);           // 3
+testArray;                       // [5, 2, 3]
--- JS210/lesson_4/array_practice.js
+++ JS210/lesson_4/array_practice.js
@@ -1,3 +1,5 @@
+// arraysEqual
+
 function arraysEqual(arr1, arr2) {
   if (arr1.length !== arr2.length) {
     return false;
@@ -20,3 +22,49 @@ console.log(arraysEqual([1, 'hi', true], [1, 'hi', true]));       // true
 console.log(arraysEqual([1, 'hi', true], [1, 'hi', false]));      // false
 console.log(arraysEqual([1, 'hi', true], [1, 'hello', true]));    // false
 console.log(arraysEqual([1, 'hi', true], [2, 'hi', true]));       // false
+
+// mirroredArray
+
+function mirroredArray(arr) {
+  return arr.concat(arr.slice().reverse());
+}
+
+// uniqueElements
+
+function uniqueElements(arr) {
+  let uniques = [];
+  let len = arr.length;
+
+  for (let index = 0; index < len; index += 1) {
+    if (uniques.indexOf(arr[index]) === -1) {
+      uniques.push(arr[index]);
+    }
+  }
+
+  return uniques;
+}
+
+console.log('uniqueElements')
+console.log(uniqueElements([1, 2, 4, 3, 4, 1, 5, 4]));  // returns [1, 2, 4, 3, 5]
+
+// missing
+
+function missing(array) {
+  let result = [];
+  let start = array[0] + 1;
+  let end = array[array.length - 1];
+
+  for (let integer = start; integer < end; integer += 1) {
+    if (array.indexOf(integer) === -1) {
+      result.push(integer);
+    }
+  }
+
+  return result;
+}
+
+console.log('missing');
+console.log(missing([-3, -2, 1, 5]));                  // [-1, 0, 2, 3, 4]
+console.log(missing([1, 2, 3, 4]));                    // []
+console.log(missing([1, 5]));                          // [2, 3, 4]
+console.log(missing([6]));                             // []
