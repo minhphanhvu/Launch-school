c29eb6f2e92e69ad09fcdf587669e5728a721aa1
minhphanhvu <minhphanh.vu@gmail.com>
2021-04-05 13:00:47 -0500

    String lesson_2

---
+++ JS215/lesson_2/string_exercise.js
@@ -0,0 +1,178 @@
+// Reverse a String
+
+function reverse(string) {
+  return string.split('').reverse().join('');
+}
+
+console.log(reverse('hello'));                  // returns "olleh"
+console.log(reverse('The quick brown fox'));    // returns "xof nworb kciuq ehT"
+
+// Acronym
+
+function acronym(string) {
+  return string.split(/[- ]/g).map(word => word[0].toUpperCase()).join('')
+}
+
+console.log(acronym('Portable Network Graphics'));                  // "PNG"
+console.log(acronym('First In, First Out'));                        // "FIFO"
+console.log(acronym('PHP: HyperText Preprocessor'));                // "PHP"
+console.log(acronym('Complementary metal-oxide semiconductor'));    // "CMOS"
+console.log(acronym('Hyper-text Markup Language'));                 // "HTML"
+
+// Email validation
+
+function isValidEmail(email) {
+  const regex = /^[a-z0-9]+@([a-z]+\.)+[a-z]+$/i;
+  if (email.match(regex)) {
+    return true;
+  }
+
+  return false;
+}
+
+console.log('Email Validation');
+console.log(isValidEmail('Foo@baz.com.ph'));          // returns true
+console.log(isValidEmail('Foo@mx.baz.com.ph'));       // returns true
+console.log(isValidEmail('foo@baz.com'));             // returns true
+console.log(isValidEmail('foo@baz.ph'));              // returns true
+console.log(isValidEmail('HELLO123@baz'));            // returns false
+console.log(isValidEmail('foo.bar@baz.to'));          // returns false
+console.log(isValidEmail('foo@baz.'));                // returns false
+console.log(isValidEmail('foo_bat@baz'));             // returns false
+console.log(isValidEmail('foo@bar.a12'));             // returns false
+console.log(isValidEmail('foo_bar@baz.com'));         // returns false
+console.log(isValidEmail('foo@bar.....com'));         // returns false
+
+// Matching Parenthesis
+
+function isBalanced(str) {
+  let count = 0;
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === '(') {
+      count += 1;
+    } else if (str[i] === ')') {
+      count -= 1;
+    }
+
+    if (count < 0) {
+      return false;
+    }
+  };
+
+  return count === 0;
+}
+
+console.log('Matching Parenthesis');
+console.log(isBalanced('What (is) this?'));        // true
+console.log(isBalanced('What is) this?'));         // false
+console.log(isBalanced('What (is this?'));         // false
+console.log(isBalanced('((What) (is this))?'));    // true
+console.log(isBalanced('((What)) (is this))?'));   // false
+console.log(isBalanced('Hey!'));                   // true
+console.log(isBalanced(')Hey!('));                 // false
+console.log(isBalanced('What ((is))) up('));       // false
+
+// Sentiment Analysis 1
+
+let textExcerpt = 'To be or not to be-that is the question:\n' +
+  'Whether \'tis nobler in the mind to suffer\n' +
+  'The slings and arrows of outrageous fortune,\n' +
+  'Or to take arms against a sea of troubles,\n' +
+  'And, by opposing, end them. To die, to sleep-\n' +
+  'No more-and by a sleep to say we end\n' +
+  'The heartache and the thousand natural shocks\n' +
+  'That flesh is heir to-\'tis a consummation\n' +
+  'Devoutly to be wished. To die, to sleep-\n' +
+  'To sleep, perchance to dream. Aye, there\'s the rub,\n' +
+  'For in that sleep of death what dreams may come,\n' +
+  'When we have shuffled off this mortal coil,\n' +
+  'Must give us pause. There\'s the respect\n' +
+  'That makes calamity of so long life.\n' +
+  'For who would bear the whips and scorns of time,\n' +
+  'Th\' oppressor\'s wrong, the proud man\'s contumely, [F: poor]\n' +
+  'The pangs of despised love, the law’s delay, [F: disprized]\n' +
+  'The insolence of office, and the spurns\n' +
+  'That patient merit of the unworthy takes,\n' +
+  'When he himself might his quietus make\n' +
+  'With a bare bodkin? Who would fardels bear, [F: these Fardels]\n' +
+  'To grunt and sweat under a weary life,\n' +
+  'But that the dread of something after death,\n' +
+  'The undiscovered country from whose bourn\n' +
+  'No traveler returns, puzzles the will\n' +
+  'And makes us rather bear those ills we have\n' +
+  'Than fly to others that we know not of?\n' +
+  'Thus conscience does make cowards of us all,\n' +
+  'And thus the native hue of resolution\n' +
+  'Is sicklied o\'er with the pale cast of thought,\n' +
+  'And enterprises of great pitch and moment, [F: pith]\n' +
+  'With this regard their currents turn awry, [F: away]\n' +
+  'And lose the name of action.-Soft you now,\n' +
+  'The fair Ophelia.-Nymph, in thy orisons\n' +
+  'Be all my sins remembered';
+
+let positiveWords = ['fortune', 'dream', 'love', 'respect', 'patience', 'devout', 'noble', 'resolution'];
+let negativeWords = ['die', 'heartache', 'death', 'despise', 'scorn', 'weary', 'trouble', 'oppress'];
+
+function sentiment(text) {
+  let wordList = text.toLowerCase().match(/[a-z']+/g);
+
+  let positives = wordList.filter(word => positiveWords.indexOf(word) >= 0);
+  let negatives = wordList.filter(word => negativeWords.indexOf(word) >= 0);
+
+  console.log('There are ' + String(positives.length) + ' positive words in the text.');
+  console.log('Positive sentiments: ' + positives.join(', '));
+  console.log('');
+  console.log('There are ' + String(negatives.length) + ' negative words in the text.');
+  console.log('Negative sentiments: ' + negatives.join(', '));
+  console.log('');
+
+  let textSentiment;
+  if (positives.length > negatives.length) {
+    textSentiment = 'Positive';
+  } else if (positives.length < negatives.length) {
+    textSentiment = 'Negative';
+  } else {
+    textSentiment = 'Neutral';
+  }
+
+  console.log('The sentiment of the text is ' + textSentiment + '.');
+}
+
+console.log('Sentinent Analysis 1');
+console.log(sentiment(textExcerpt));
+
+// console output
+
+// There are 5 positive words in the text.
+// Positive sentiments: fortune, dream, respect, love, resolution
+
+// There are 6 negative words in the text.
+// Negative sentiments: die, heartache, die, death, weary, death
+
+// The sentiment of the text is Negative.
+
+// MailCount
+function mailCount(emailData) {
+  let emails = emailData.split('##||##');
+  let count = emails.length;
+  let emailDates = emails.map(email => email.split('#/#')[2]);
+
+  console.log('Count of Email: ' + count);
+  console.log('Date Range: ' + displayableDateRange(emailDates));
+}
+
+function displayableDateRange(dates) {
+  let dateObjects = getDateObjects(dates);
+  dateObjects.sort((a, b) => a.valueOf() - b.valueOf());
+  return dateObjects[0].toDateString() + ' - ' + dateObjects[dateObjects.length - 1].toDateString();
+}
+
+function getDateObjects(dates) {
+  return dates.map(date => {
+    let dateElements = date.split(' ')[1].split('-');
+    let month = parseInt(dateElements[0], 10) - 1;
+    let day = parseInt(dateElements[1], 10);
+    let year = parseInt(dateElements[2], 10);
+    return new Date(year, month, day);
+  });
+}
