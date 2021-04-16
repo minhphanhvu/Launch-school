cd1089bc051285616855f80e1853cc28d93b6bf5
minhphanhvu <minhphanh.vu@gmail.com>
2021-04-16 15:06:48 -0500

    Add some small javascript problems preparing JS215 test

---
+++ JS215/problems/removeVowels.js
@@ -0,0 +1,11 @@
+function removeVowelsInStr(str) {
+  return str.replace(/[aioue]/ig, '')
+}
+
+function removeVowels(arr) {
+  return arr.map(removeVowelsInStr) 
+}
+
+console.log(removeVowels(['abcdefghijklmnopqrstuvwxyz']));         // ["bcdfghjklmnpqrstvwxyz"]
+console.log(removeVowels(['green', 'YELLOW', 'black', 'white']));  // ["grn", "YLLW", "blck", "wht"]
+console.log(removeVowels(['ABC', 'AEIOU', 'XYZ']));                // ["BC", "", "XYZ"]
---
+++ JS215/problems/sequence.js
@@ -0,0 +1,14 @@
+function sequence(count, start) {
+  const result = []
+
+  for (let i = 0; i < count; i++) {
+    result.push(start*(i + 1))
+  }
+
+  return result
+}
+
+console.log(sequence(5, 1));          // [1, 2, 3, 4, 5]
+console.log(sequence(4, -7));         // [-7, -14, -21, -28]
+console.log(sequence(3, 0));          // [0, 0, 0]
+console.log(sequence(0, 1000000));    // []
