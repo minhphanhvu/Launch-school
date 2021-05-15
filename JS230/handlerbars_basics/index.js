1748972619a3510d4a1884d1f0ac0deff62eed38
Minh Vu <minhphanh.vu@gmail.com>
2021-05-15 12:36:03 -0500

    Handlebars practice

---
+++ JS230/handlerbars_basics/index.js
@@ -0,0 +1,21 @@
+$(function() {
+  let posts = [
+    {
+      title: 'voluptate velit',
+      published: 'April 2, 2015',
+      body: 'At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.'
+    }
+  ];
+  let post = {
+    title: 'Lorem ipsum dolor sit amet',
+    published: 'April 1, 2015',
+    body: '<p>' + 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit.' + '</p>',
+    tags: ['a', 'b', 'c']
+  };
+  posts.push(post)
+  
+  let postTemplate = Handlebars.compile($('#posts').html());
+  Handlebars.registerPartial('tag', $('#tag').html());
+  
+  $('body').append(postTemplate({ posts: posts }));
+})
