cc4ca24b7bb47bed8f5a8da301acc16f73cea9d1
Minh Vu <minhphanh.vu@gmail.com>
2021-05-20 10:04:27 -0500

    Add custom confirmation for todo list

--- JS230_DOM_manipulation/custom_delete_confirmation_prompt/todo.js
+++ JS230_DOM_manipulation/custom_delete_confirmation_prompt/todo.js
@@ -15,14 +15,53 @@ document.addEventListener("DOMContentLoaded", () => {
     constructor(templates, todos) {
       this.templates = templates;
       this.todos = todos;
+      this.confirmPrompt = document.querySelector(".confirm_prompt");
     }
     initTodos() {
-      document.querySelector("#todos").insertAdjacentHTML("beforeend", this.templates.todos_template({ todos: this.todos }));
+      document.querySelector("#todos")
+        .insertAdjacentHTML("beforeend", this.templates.todos_template({ todos: this.todos }));
     }
-    addDeleteListener() {
-      
+    addDeleteListeners() {
+      [...document.querySelectorAll(".remove")].forEach(x => {
+        x.addEventListener("click", (e) => {
+          let todoId = e.target.closest("li").dataset.id;
+          this.displayDeleteConfirmation.call(this, todoId);
+        })
+      })
+    }
+    displayDeleteConfirmation(id) {
+      this.confirmPrompt
+        .insertAdjacentHTML("beforeend", this.templates.confirm_template({id: id}));
+      this.showConfirmPrompt.call(this);
+      this.addConfirmationListeners.call(this, id);
+    }
+    addConfirmationListeners(id) {
+      let [confirmYes, confirmNo] = [...document.querySelectorAll(".actions a")];
+      confirmNo.addEventListener("click", (e) => {
+        e.preventDefault();
+        this.removeConfirmPrompt.call(this);
+      });
+      confirmYes.addEventListener("click", (e) => {
+        e.preventDefault();
+        this.removeTodo(id)
+      })
+    }
+    showConfirmPrompt() {
+      this.confirmPrompt.classList.remove("hide");
+    }
+    removeConfirmPrompt() {
+      this.confirmPrompt.textContent = "";
+      this.confirmPrompt.classList.add("hide");
+    }
+    removeTodo(id) {
+      document.querySelector(`li[data-id='${id}']`).remove();
+      this.reset();
+    }
+    reset() {
+      this.removeConfirmPrompt.call(this);
     }
   }
   const app = new App(templates, todo_items);
   app.initTodos.call(app);
+  app.addDeleteListeners.call(app);
 })
